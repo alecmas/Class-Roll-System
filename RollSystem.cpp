40f0edd95a7f28cefba7f18a0c807921a6fcfd0b
Alec Mas <alecmas@gmail.com>
2016-08-29 10:57:55 -0400

    Add files via upload

---
+++ RollSystem.cpp
@@ -0,0 +1,21 @@
+//*****************************************************************
+//  RollSystem.cpp
+//	A small class-roll maintenance system
+//
+//  Programmers: Ryan Binder, Constance Luong, Elin Cheung, 
+//				 Wesam Ali, Alexander Mas
+//
+//  This file contains the student class and all functions
+//*****************************************************************
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	cout << "This is going to be a class-roll maintenance system." << endl;
+
+	return 0;
+}
