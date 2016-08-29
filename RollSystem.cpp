b2cf871f65451a16a933206b1f1e612c14fb5f04
Alec Mas <alecmas@gmail.com>
2016-08-29 11:19:37 -0400

    Update RollSystem.cpp

--- RollSystem.cpp
+++ RollSystem.cpp
@@ -13,6 +13,49 @@
 
 using namespace std;
 
+//******************************************************************
+// Student class will contain all of the data required:
+// name, usfId, email, presGrade, e1Grade, e2Grade, projGrade
+//******************************************************************
+
+//******************************************************************
+// displayStudents function will display all students currently 
+// in the system, as well as their data
+//******************************************************************
+
+//******************************************************************
+// addStudent function will create a student
+// and add that student to the system
+//******************************************************************
+
+//******************************************************************
+// searchByName function will search system for student
+// by their name
+//
+// !! Need to consider duplicates !!
+//******************************************************************
+
+//******************************************************************
+// searchById function will search system for student 
+// by their UID
+//******************************************************************
+
+//******************************************************************
+// searchByEmail function will search system for student
+// by their email
+//******************************************************************
+
+//******************************************************************
+// editName function will edit student's name
+//******************************************************************
+
+
+//******************************************************************
+// editStudent function will find student by searchById
+// and then prompt the user for what field they would like to edit:
+// editName, editId, editEmail, editGrades
+//******************************************************************
+
 int main() {
 
 	cout << "This is going to be a class-roll maintenance system." << endl;
